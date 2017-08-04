a23a21b16253d9aa0a092d603b80d07ef8571a3a
dmitryskey <dmitryskey@hotmail.com>
2017-08-03 20:56:01 -0400

    Help icons, localization, Section 1 logic

--- templates/src/US I9.js
+++ templates/src/US I9.js
@@ -1,4 +1,4 @@
-var $ = require(['jQuery', 'jQueryUI']);
+var $ = require(['jQueryUI', 'jQuery']);
 
 class USI9 {
     get _() {
@@ -17,21 +17,25 @@ class USI9 {
 
     constructor() {
         this.nameFormat = /^[A-Za-z ']+$/;
+        this.NAFormat = /^[NA/']+$/;
         this.zipFormat = /^[\d-]+$/;
         this.zipNumber = /^(\d{5})(-\d{4}){0,1}$/;
         this.dateFormat = /^\d{2}[/]{1}\d{2}[/]{1}\d{4}$/;
         this.numberFormat = /^\d{1}$/;
-        this.phoneFormat = /^[\d \-()]+$/;
+        this.phoneFormat = /^[\d/NA-]+$/;
         this.phoneNumber = /^[(]{0,1}\d{3}[ )-]{0,1}\d{3}[ -]{0,1}\d{4}$/;
     }
 
-    renderPage1(dialog,
+    renderSection1(dialog,
         lastName, lastNameHelp, firstName, firstNameHelp,
-        middleInitial, 
-        otherNames, address, apptNumber, city,
-        state, zip, dob, ssn11, ssn12, ssn13,
-        ssn21, ssn22, ssn31, ssn32, ssn33, ssn34,
-        email, phone, citizen, national, lpr, alien,
+        middleInitial, middleInitialHelp,
+        otherNames, otherNamesHelp,
+        address, addressHelp, apptNumber, apptNumberHelp,
+        city, cityHelp, state, stateHelp, zip, zipHelp,
+        dob, dobHelp, ssn11, ssn12, ssn13, ssn21, ssn22, ssn31,
+        ssn32, ssn33, ssn34, ssnHelp,
+        email, emailHelp, phone, phoneHelp,
+        citizen, national, lpr, alien,
         lpruscisNum, lpruscisNumType, alienWorkAuthDate,
         alienuscisNum, alienuscisNumType, admissionNum,
         passportNum, countryOfIssuance) {
@@ -40,11 +44,18 @@ class USI9 {
             autoOpen: false
         });
 
+        $.datepicker.setDefaults($.datepicker.regional[this._('jquery.locale')]);
+
+        $('a').prop('target', '_blank');
+
         // E-Verify requirements
         this._lastName = lastName;
         this._lastName.prop('maxLength', 40);
         this._lastName.keypress(e =>
             this.nameFormat.test(String.fromCharCode(e.which)));
+        this._lastName.blur(() => 
+            this._lastNameSection2.val(this._lastName.val())
+        );
 
         this._lastNameHelp = lastNameHelp;
         this.renderHelpIcon(
@@ -59,6 +70,9 @@ class USI9 {
         this._firstName.prop('maxLength', 25);
         this._firstName.keypress(e =>
             this.nameFormat.test(String.fromCharCode(e.which)));
+        this._firstName.blur(() => 
+            this._firstNameSection2.val(this._firstName.val())
+        );
 
         this._firstNameHelp = firstNameHelp;
         this.renderHelpIcon(
@@ -68,21 +82,65 @@ class USI9 {
             this._('firstnamehelp.text')
         );
 
-        // E-Verify requirements
+        // E-Verify requirements + N/A option
         this._middleInitial = middleInitial;
-        this._middleInitial.prop('maxLength', 1);
+        this._middleInitial.prop('maxLength', 3);
         this._middleInitial.keypress(e =>
-            this.nameFormat.test(String.fromCharCode(e.which)));
+            this.nameFormat.test(String.fromCharCode(e.which)) ||
+            this.NAFormat.test(String.fromCharCode(e.which)));
+        this._middleInitial.blur(() => 
+            this._middleInitialSection2.val(this._middleInitial.val())
+        );
+
+        this._middleInitialHelp = middleInitialHelp;
+        this.renderHelpIcon(
+            this._middleInitialHelp,
+            this._('middleinitialhelp.caption'),
+            dialog,
+            this._('middleinitialhelp.text')
+        );
 
         // E-Verify requirements
         this._otherNames = otherNames;
         this._otherNames.prop('maxLength', 40);
         this._otherNames.keypress(e =>
-            this.nameFormat.test(String.fromCharCode(e.which)));
+            this.nameFormat.test(String.fromCharCode(e.which)) ||
+            this.NAFormat.test(String.fromCharCode(e.which)));
+
+        this._otherNamesHelp = otherNamesHelp;
+        this.renderHelpIcon(
+            this._otherNamesHelp,
+            this._('othernameshelp.caption'),
+            dialog,
+            this._('othernameshelp.text')
+        );
 
         this._address = address;
+        this._addressHelp = addressHelp;
+        this.renderHelpIcon(
+            this._addressHelp,
+            this._('addresshelp.caption'),
+            dialog,
+            this._('addresshelp.text')
+        );
+
         this._apptNumber = apptNumber;
+        this._apptNumberHelp = apptNumberHelp;
+        this.renderHelpIcon(
+            this._apptNumberHelp,
+            this._('apartmentnumberhelp.caption'),
+            dialog,
+            this._('apartmentnumberhelp.text')
+        );
+
         this._city = city;
+        this._cityHelp = cityHelp;
+        this.renderHelpIcon(
+            this._cityHelp,
+            this._('cityhelp.caption'),
+            dialog,
+            this._('cityhelp.text')
+        );
 
         this._state = state;
         this._state.empty();
@@ -90,13 +148,39 @@ class USI9 {
             this._state.append('<option>' + stateCode + '</option>');
         }
 
+        this._stateHelp = stateHelp;
+        this.renderHelpIcon(
+            this._stateHelp,
+            this._('statehelp.caption'),
+            dialog,
+            this._('statehelp.text')
+        );
+
         this._zip = zip;
         this._zip.keypress(e =>
             this.zipFormat.test(String.fromCharCode(e.which)));
 
+        this._zipHelp = zipHelp;
+        this.renderHelpIcon(
+            this._zipHelp,
+            this._('ziphelp.caption'),
+            dialog,
+            this._('ziphelp.text')
+        );
+
         // E-Verify requirements
         this._dob = dob;
-        this._dob.datepicker();
+        this._dob.datepicker({
+            dateFormat: 'dd-mm-yy'
+        });
+
+        this._dobHelp = dobHelp;
+        this.renderHelpIcon(
+            this._dobHelp,
+            this._('dobhelp.caption'),
+            dialog,
+            this._('dobhelp.text')
+        );
 
         this._ssn11 = ssn11;
         this._ssn11.prop('maxLength', 1);
@@ -143,15 +227,39 @@ class USI9 {
         this._ssn34.keypress(e =>
             this.numberFormat.test(String.fromCharCode(e.which)));
 
+        this._ssnHelp = ssnHelp;
+        this.renderHelpIcon(
+            this._ssnHelp,
+            this._('ssnhelp.caption'),
+            dialog,
+            this._('ssnhelp.text')
+        );
+
         this._email = email;
-        this._email.prop('maxLength', 50);
+        this._email.prop('maxLength', 60);
         this._email.keypress(e =>
             this.numberFormat.test(String.fromCharCode(e.which)));
+
+        this._emailHelp = emailHelp;
+        this.renderHelpIcon(
+            this._emailHelp,
+            this._('emailhelp.caption'),
+            dialog,
+            this._('emailhelp.text')
+        );
 
         this._phone = phone;
         this._phone.prop('maxLength', 13);
         this._phone.keypress(e =>
             this.phoneFormat.test(String.fromCharCode(e.which)));
+
+        this._phoneHelp = phoneHelp;
+        this.renderHelpIcon(
+            this._phoneHelp,
+            this._('phonehelp.caption'),
+            dialog,
+            this._('phonehelp.text')
+        );
 
         var arr = [citizen, national, lpr, alien];
 
@@ -160,6 +268,7 @@ class USI9 {
             this.selectCheckmark(this._citizen, arr);
             this.processLPR(this._citizen.prop('checked'));
             this.processAlien(this._citizen.prop('checked'));
+            this._immigrationStatus.val(this._citizen.prop('checked') ? '1' : 0);
         });
 
         this._national = national;
@@ -167,12 +276,14 @@ class USI9 {
             this.selectCheckmark(this._national, arr);
             this.processLPR(this._national.prop('checked'));
             this.processAlien(this._national.prop('checked'));
+            this._immigrationStatus.val(this._national.prop('checked') ? '2' : 0);
         });
 
         this._lpr = lpr;
         this._lpr.click(() => {
             this.selectCheckmark(this._lpr, arr);
             this.processAlien(this._lpr.prop('checked'));
+            this._immigrationStatus.val(this._lpr.prop('checked') ? '3' : 0);
         });
 
         this._alien = alien;
@@ -184,17 +295,51 @@ class USI9 {
             for (var c in countries) {
                 this._countryOfIssuance.append('<option value="' + c + '">' + countries[c] + '</option>');
             }
+            this._immigrationStatus.val(this._alien.prop('checked') ? '4' : 0);
         });
 
         this._lpruscisNum = lpruscisNum;
+        this._lpruscisNum.prop('maxLength', 9);
+        this._lpruscisNum.keypress(e =>
+            this.numberFormat.test(String.fromCharCode(e.which)));
+
         this._lpruscisNumType = lpruscisNumType;
+        
         this._alienWorkAuthDate = alienWorkAuthDate;
+        this._alienWorkAuthDate.datepicker();
         this._alienuscisNum = alienuscisNum;
+        this._alienuscisNum.prop('maxLength', 9);
+        this._alienuscisNum.keypress(e =>
+            this.numberFormat.test(String.fromCharCode(e.which)));
+
         this._alienuscisNumType = alienuscisNumType;
 
         this._admissionNum = admissionNum;
+        this._admissionNum.prop('maxLength', 11);
+        this._admissionNum.keypress(e =>
+            this.numberFormat.test(String.fromCharCode(e.which)));
+
+        // E-Verify requirements
         this._passportNum = passportNum;
+        this._passportNum.prop('maxLength', 12);
+
         this._countryOfIssuance = countryOfIssuance;
+    }
+
+    renderSection2(dialog,
+        lastName, lastNameHelp, firstName, firstNameHelp,
+        middleInitial, middleInitialHelp,
+        immigrationStatus, immigrationStatusHelp,
+        listADoc, listBDoc, listCDoc) {
+
+        dialog.dialog({
+            autoOpen: false
+        });
+
+        this._lastNameSection2 = lastName;
+        this._firstNameSection2 = firstName;
+        this._middleInitialSection2 = middleInitial;
+        this._immigrationStatus = immigrationStatus;
     }
 
     validateFields() {
@@ -300,20 +445,28 @@ document.addEventListener('pagerendered', function (e) {
         $('body').append('<div id="dialogPage' + e.detail.pageNumber + '"></div>');
         $(document).tooltip();
 
-        form.renderPage1(
+        form.renderSection1(
             $('#dialogPage' + e.detail.pageNumber),
             $('[name=LastName]'),
             $('[name=LastNameHelp]'),
             $('[name=FirstName]'),
             $('[name=FirstNameHelp]'),
             $('[name=MiddleInitial]'),
+            $('[name=MiddleInitialHelp]'),
             $('[name=OtherNames]'),
+            $('[name=OtherNamesHelp]'),
             $('[name=Address]'),
-            $('[name=AppartmentNumber]'),
+            $('[name=AddressHelp]'),
+            $('[name=ApartmentNumber]'),
+            $('[name=ApartmentNumberHelp]'),
             $('[name=City]'),
+            $('[name=CityHelp]'),
             $('[name=State]'),
+            $('[name=StateHelp]'),
             $('[name=Zip]'),
+            $('[name=ZipHelp]'),
             $('[name=DateOfBirth]'),
+            $('[name=DateOfBirthHelp]'),
             $('[name=SSN11]'),
             $('[name=SSN12]'),
             $('[name=SSN13]'),
@@ -323,8 +476,11 @@ document.addEventListener('pagerendered', function (e) {
             $('[name=SSN32]'),
             $('[name=SSN33]'),
             $('[name=SSN34]'),
-            $('[name=EmailAddress]'),
-            $('[name=PhoneNumber]'),
+            $('[name=SSNHelp]'),
+            $('[name=Email]'),
+            $('[name=EmailHelp]'),
+            $('[name=Phone]'),
+            $('[name=PhoneHelp]'),
             $('[name=Citizen]'),
             $('[name=NonCitizenNational]'),
             $('[name=LawfulPermanentResident]'),
@@ -337,6 +493,26 @@ document.addEventListener('pagerendered', function (e) {
             $('[name=AdmissionNumber]'),
             $('[name=ForeignPassportNumber]'),
             $('[name=CountryOfIssuance]')
+        );
+    }
+
+    if (e.detail.pageNumber === 2) {
+        $('body').append('<div id="dialogPage' + e.detail.pageNumber + '"></div>');
+        $(document).tooltip();
+
+        form.renderSection2(
+            $('#dialogPage' + e.detail.pageNumber),
+            $('[name=LastNameSection2]'),
+            $('[name=LastNameSection2Help]'),
+            $('[name=FirstNameSection2]'),
+            $('[name=FirstNameSection2Help]'),
+            $('[name=MiddleInitialSection2]'),
+            $('[name=MiddleInitialSection2Help]'),
+            $('[name=ImmigrationStatus]'),
+            $('[name=ImmigrationStatusHelp]'),
+            $('[name=ListADocTitle]'),
+            $('[name=ListBDocTitle]'),
+            $('[name=ListCDocTitle]')
         );
     }
 }, true);
